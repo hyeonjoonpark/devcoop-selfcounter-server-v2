cf4b80555faadf62f2f7fa325c2078e463cf6fb1
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-22 22:43:52 +0900

    FEAT(payLog): Paylog 엔티티 생성
    엔티티 파일을 생성하였습니다

---
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/item/enums/EventType.kt
@@ -0,0 +1,6 @@
+package devcoop.hyunjoon.selfcounter.domain.item.enums
+
+enum class EventType(val description: String) {
+    ONE_PLUS_ONE("1+1행사"),
+    NONE("행사상품아님")
+}
---
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/paylog/Paylog.kt
@@ -0,0 +1,30 @@
+package com.devcoop.kiosk.domain.paylog
+
+import devcoop.hyunjoon.selfcounter.domain.item.enums.EventType
+import devcoop.hyunjoon.selfcounter.domain.user.User
+import jakarta.persistence.*
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@Entity @Table(name = "occount_payLog")
+@EntityListeners(AuditingEntityListener::class)
+class PayLog(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var payId: Long = 0L,
+    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "userCode", nullable = false)
+    private var _user: User,
+    @CreatedDate var payDate: LocalDateTime = LocalDateTime.now(),
+    var payType: String,
+    var beforePoint: Int,
+    var payedPoint: Int,
+    var afterPoint: Int,
+    val managedEmail: String = "셀프결제",
+    @Enumerated(EnumType.STRING) var eventType: EventType,
+) {
+    var user: User
+        get() = _user
+        set(value) {
+            _user = value
+        }
+}
