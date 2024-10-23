aca443fc29cdf47bbd8150ad3abe375a20224675
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-23 10:58:59 +0900

    FIX(user) : User 엔티티

--- src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/User.kt
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/User.kt
@@ -31,14 +31,32 @@ class User private constructor(
         protected set
     @Column(name = "userFingerPrint") var userFingerPrint: String = userFingerPrint
         protected set
-    @Enumerated(value = EnumType.STRING) @Column(name = "roles") var role: Role = Role.ROLE_USER
+    @Enumerated(value = EnumType.STRING) @Column(name = "role")
+    var role: Role = Role.ROLE_USER
         protected set
+    @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true)
+    private val _payLogs: MutableList<PayLog> = mutableListOf()
 
-    @OneToMany(mappedBy = "_user", cascade = [CascadeType.ALL], orphanRemoval = true)
-    var payLogs: MutableList<PayLog> = mutableListOf()
+    val payLogs: List<PayLog>
+        get() = _payLogs
 
     fun addPaylog(payLog: PayLog) {
-        payLogs.add(payLog)
-        this.payLogs.add(payLog)
+        payLog.user = this // PayLog의 user를 현재 User로 설정
+        _payLogs.add(payLog) // PayLog를 리스트에 추가
+    }
+
+    companion object {
+        fun create(
+            id: String,
+            userCode: String,
+            userName: String,
+            userEmail: String,
+            userPassword: String,
+            userPin: String,
+            userPoint: Int,
+            userFingerPrint: String
+        ): User {
+            return User(id, userCode, userName, userEmail, userPassword, userPin, userPoint, userFingerPrint)
+        }
     }
 }
