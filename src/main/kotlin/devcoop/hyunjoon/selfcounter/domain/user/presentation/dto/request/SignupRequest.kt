f02435f540e63b1bf16dc660f185a60912594d74
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-26 00:22:21 +0900

    FEAT: dto 생성

--- src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/presentation/dto/request/SignupRequest.kt
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/presentation/dto/request/SignupRequest.kt
@@ -1,3 +1,10 @@
 package devcoop.hyunjoon.selfcounter.domain.user.presentation.dto.request
 
-data class SignupRequest()
+data class SignupRequest(
+    val userNumber: String,
+    val userCode: String,
+    val userName: String,
+    val userEmail: String,
+    val userPassword: String,
+    val userPin: String
+)
