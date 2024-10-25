2c274fca6be0a6f4ea7baaac21fa9bf10d7d1d64
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-26 00:21:53 +0900

    FEAT(USER): signUp API 컨트롤러 생성

--- src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/presentation/UserController.kt
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/presentation/UserController.kt
@@ -1,10 +1,13 @@
 package devcoop.hyunjoon.selfcounter.domain.user.presentation
 
+import devcoop.hyunjoon.selfcounter.domain.user.presentation.dto.request.SignupRequest
 import devcoop.hyunjoon.selfcounter.domain.user.presentation.dto.request.UserPointRequest
+import devcoop.hyunjoon.selfcounter.domain.user.service.UserRepository
 import devcoop.hyunjoon.selfcounter.domain.user.service.UserService
 import devcoop.hyunjoon.selfcounter.global.utils.ApiPath
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,9 +15,17 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(ApiPath.COUNTER_USER_API_URL)
-class UserController(val userService: UserService) {
+class UserController(
+    val userService: UserService,
+    private val userRepository: UserRepository
+) {
     @PutMapping("/pay")
     fun pay(@RequestBody dto: UserPointRequest): ResponseEntity<Any> {
         return ResponseEntity.status(HttpStatus.OK).body(null)
+    }
+
+    @PostMapping("/signUp")
+    fun signUp(@RequestBody dto: SignupRequest): ResponseEntity<Any> {
+        return userService.signUp(dto);
     }
 }
