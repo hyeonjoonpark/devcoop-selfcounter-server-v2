0c77ed3538209ff8b06eb36dc6318b6115ee72d2
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-26 22:39:30 +0900

    ADD: UserRepository Query Method

--- src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/repository/UserRepository.kt
@@ -2,7 +2,8 @@ package devcoop.hyunjoon.selfcounter.domain.user.service
 
 import org.springframework.data.jpa.repository.JpaRepository
 import devcoop.hyunjoon.selfcounter.domain.user.User
+import java.util.Optional
 
 interface UserRepository : JpaRepository<User, String> {
-    
+    fun findByUserCode(userCode: String): Optional<User>
 }
