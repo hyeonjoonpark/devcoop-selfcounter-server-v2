3c9e5c30122fa7de3005dc4ab718990b979eb187
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-26 10:16:01 +0900

    ADD: SigninResponse
    로그인 성공 시 응답 DTO를 생성하였습니다

---
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/presentation/dto/request/SigninResponse.kt
@@ -0,0 +1,12 @@
+package devcoop.hyunjoon.selfcounter.domain.user.presentation.dto.request
+
+import jakarta.validation.constraints.NotBlank
+
+data class SigninResponse(
+    @field:NotBlank val message: String,
+    @field:NotBlank val userCode: String,
+    @field:NotBlank val userName: String,
+    val userPoint: Int,
+    @field:NotBlank val accessToken: String,
+    @field:NotBlank val refreshToken: String,
+)
