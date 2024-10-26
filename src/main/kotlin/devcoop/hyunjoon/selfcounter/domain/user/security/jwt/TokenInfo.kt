515b5865f8717f43302625151a07434ebec606cf
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-26 22:41:26 +0900

    .

--- src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/security/jwt/TokenInfo.kt
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/security/jwt/TokenInfo.kt
@@ -1,3 +1,16 @@
 package devcoop.hyunjoon.selfcounter.domain.user.security.jwt
 
-data class TokenInfo()
+data class TokenInfo(
+    val grantType: TokenGrantType = TokenGrantType.BEARER,
+    val accessToken: String,
+    val refreshToken: String,
+) {
+    enum class TokenGrantType(val value: String) {
+        BEARER("Bearer")
+    }
+
+    enum class TokenType(val value: String) {
+        ACCESS("access"),
+        REFRESH("refresh")
+    }
+}
