e2c4c98f8537fc2d271cd0d0686cecbc2d412c01
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-26 14:18:00 +0900

    REMOVE: Dto에 필요없는 인자 삭제

--- src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/presentation/dto/request/SignupRequest.kt
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/presentation/dto/request/SignupRequest.kt
@@ -1,7 +1,6 @@
 package devcoop.hyunjoon.selfcounter.domain.user.presentation.dto.request
 
 data class SignupRequest(
-    val userNumber: String,
     val userCode: String,
     val userName: String,
     val userEmail: String,
