251ff895c4adc3eb1965af619b2ca5d1650c2114
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-26 21:21:08 +0900

    FEAT(filter): JwtFilter

--- src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/security/filter/JwtFilter.kt
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/security/filter/JwtFilter.kt
@@ -1,12 +1,12 @@
 package devcoop.hyunjoon.selfcounter.domain.user.security.filter
 
+import devcoop.hyunjoon.selfcounter.domain.user.security.CustomUserDetailsService
 import devcoop.hyunjoon.selfcounter.global.utils.JwtUtil
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
@@ -14,7 +14,7 @@ import org.springframework.web.filter.OncePerRequestFilter
 @Component
 class JwtFilter(
     private val jwtUtil: JwtUtil,
-    private val userDetailsService: UserDetailsService
+    private val customUserDetailsService: CustomUserDetailsService
 ) : OncePerRequestFilter() {
 
     override fun doFilterInternal(
@@ -24,18 +24,17 @@ class JwtFilter(
     ) {
         val authorizationHeader = request.getHeader("Authorization")
 
-        var username: String? = null
-        var jwt: String? = null
+        lateinit var userName: String
+        lateinit var jwt: String
 
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7)
-            username = jwtUtil.extractUsername(jwt)
+            userName = jwtUtil.extractUsername(jwt)
         }
 
-        if (username != null && SecurityContextHolder.getContext().authentication == null) {
-            val userDetails = userDetailsService.loadUserByUsername(username)
-
-            if (jwtUtil.validateToken(jwt!!, userDetails)) {
+        if (SecurityContextHolder.getContext().authentication == null) {
+            val userDetails = customUserDetailsService.loadUserByUsername(userName)
+            if (jwtUtil.validateToken(jwt, userDetails)) {
                 val usernamePasswordAuthenticationToken = UsernamePasswordAuthenticationToken(
                     userDetails, null, userDetails.authorities
                 )
