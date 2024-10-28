6929a4196b64a0dc862bc59202cd6cd2872948af
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-28 12:04:29 +0900

    .

---
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/item/presentation/ItemController.kt
@@ -0,0 +1,21 @@
+package devcoop.hyunjoon.selfcounter.domain.item.presentation
+
+import devcoop.hyunjoon.selfcounter.domain.item.presentation.dto.response.ItemResponse
+import devcoop.hyunjoon.selfcounter.domain.item.service.ItemService
+import devcoop.hyunjoon.selfcounter.domain.user.service.UserService
+import devcoop.hyunjoon.selfcounter.global.utils.ApiPath
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(ApiPath.COUNTER_ITEM_API_URL)
+class ItemController(
+    val itemService: ItemService,
+) {
+    @GetMapping
+    fun readItem(@RequestBody barcodes: List<String>): List<ItemResponse> {
+        return itemService.readByBarcode(barcodes)
+    }
+}
