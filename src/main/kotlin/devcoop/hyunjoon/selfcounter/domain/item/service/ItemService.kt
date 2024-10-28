de0678bd4833376b40ea726024b94afabe9d651f
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-28 12:04:13 +0900

    FEAT: readItem 함수생성

--- src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/item/service/ItemService.kt
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/item/service/ItemService.kt
@@ -1,8 +1,11 @@
 package devcoop.hyunjoon.selfcounter.domain.item.service
 
+import devcoop.hyunjoon.selfcounter.domain.item.presentation.dto.response.ItemResponse
 import org.springframework.stereotype.Service
 
 @Service
 class ItemService {
+    fun readByBarcode(barcodes: List<String>): List<ItemResponse> {
 
+    }
 }
