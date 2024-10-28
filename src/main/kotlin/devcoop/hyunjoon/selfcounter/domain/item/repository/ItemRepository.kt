5cad9da9facb1417f170e70dca3798875710148a
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-28 13:21:23 +0900

    FEAT(item): Item 바코드로 항목조회 API

--- src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/item/repository/ItemRepository.kt
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/item/repository/ItemRepository.kt
@@ -2,7 +2,8 @@ package devcoop.hyunjoon.selfcounter.domain.item.repository
 
 import devcoop.hyunjoon.selfcounter.domain.item.Item
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
 interface ItemRepository: JpaRepository<Item, Long> {
-
+    fun findByItemCode(itemCode: String): Optional<Item>
 }
--- src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/item/service/ItemService.kt
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/item/service/ItemService.kt
@@ -1,12 +1,39 @@
 package devcoop.hyunjoon.selfcounter.domain.item.service
 
+import devcoop.hyunjoon.selfcounter.domain.item.Item
+import devcoop.hyunjoon.selfcounter.domain.item.enums.EventType
 import devcoop.hyunjoon.selfcounter.domain.item.presentation.dto.response.ItemResponse
+import devcoop.hyunjoon.selfcounter.domain.item.repository.ItemRepository
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 @Service
-class ItemService {
+class ItemService(
+    private val itemRepository: ItemRepository
+) {
+    var quantity = 1
+    var eventType = EventType.NONE
     fun readByBarcode(barcodes: List<String>): ResponseEntity<MutableList<ItemResponse>> {
+        val itemResponses: MutableList<ItemResponse> = mutableListOf()
 
+        for (barcode in barcodes) {
+            val item: Item = itemRepository.findByItemCode(barcode)
+                .orElseThrow{RuntimeException("존재하지 않는 상품입니다")}
+
+            if(item.event == EventType.ONE_PLUS_ONE) {
+                quantity = 2
+                eventType = EventType.ONE_PLUS_ONE
+            }
+
+            val response = ItemResponse(
+                itemName = item.itemName,
+                itemPrice = item.itemPrice,
+                itemQuantity = item.itemQuantity,
+                eventType = eventType
+            )
+            itemResponses.add(response)
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(itemResponses)
     }
 }
