3b4c66667b815dbf4a9d89fa827aa7e509e1935b
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-28 12:13:08 +0900

    FIX: 타입수정

--- src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/item/presentation/ItemController.kt
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/item/presentation/ItemController.kt
@@ -3,6 +3,7 @@ package devcoop.hyunjoon.selfcounter.domain.item.presentation
 import devcoop.hyunjoon.selfcounter.domain.item.presentation.dto.response.ItemResponse
 import devcoop.hyunjoon.selfcounter.domain.item.service.ItemService
 import devcoop.hyunjoon.selfcounter.global.utils.ApiPath
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,7 +15,7 @@ class ItemController(
     val itemService: ItemService,
 ) {
     @GetMapping("/read")
-    fun readItem(@RequestBody barcodes: List<String>): List<ItemResponse> {
+    fun readItem(@RequestBody barcodes: List<String>): ResponseEntity<List<ItemResponse>> {
         return itemService.readByBarcode(barcodes)
     }
 }
--- src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/item/service/ItemService.kt
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/item/service/ItemService.kt
@@ -1,11 +1,12 @@
 package devcoop.hyunjoon.selfcounter.domain.item.service
 
 import devcoop.hyunjoon.selfcounter.domain.item.presentation.dto.response.ItemResponse
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 @Service
 class ItemService {
-    fun readByBarcode(barcodes: List<String>): List<ItemResponse> {
+    fun readByBarcode(barcodes: List<String>): ResponseEntity<List<ItemResponse>> {
 
     }
 }
