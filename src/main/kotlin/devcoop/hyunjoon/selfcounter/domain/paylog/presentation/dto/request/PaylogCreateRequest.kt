221662fa6b27cfae9ffeadcdcdd92dd2c521f4ba
hyeonjoonpark <pjjoon1379@gmail.com>
2024-11-11 19:44:53 +0900

    FEAT: Paylog 생성 로직
    validate 함수를 만들어 검증로직 분리해서 코드 작성
    추후 검증로직 더 있으면 추가 예정

--- src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/paylog/presentation/dto/request/PaylogCreateRequest.kt
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/paylog/presentation/dto/request/PaylogCreateRequest.kt
@@ -2,13 +2,14 @@ package devcoop.hyunjoon.selfcounter.domain.paylog.presentation.dto.request
 
 import devcoop.hyunjoon.selfcounter.domain.item.enums.EventType
 import devcoop.hyunjoon.selfcounter.domain.paylog.PayLog
-import java.time.LocalDateTime
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Positive
 
 data class PaylogCreateRequest(
-    val beforePoint: Int,
-    val afterPoint: Int,
-    val userCode: String,
-    val payedPoint: Int,
+    @Positive(message = "결제금액은 음수일 수 없습니다") val beforePoint: Int,
+    @Positive(message = "결제금액은 음수일 수 없습니다") val afterPoint: Int,
+    @NotBlank(message = "사용자코드는 필수값입니다") val userCode: String,
+    @Positive(message = "결제금액은 음수일 수 없습니다") val payedPoint: Int,
     val payType: String,
     val managedEmail: String,
     val eventType: EventType,
--- src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/user/repository/UserRepository.kt
@@ -1,11 +1,15 @@
 package devcoop.hyunjoon.selfcounter.domain.user.service
 
-import org.springframework.data.jpa.repository.JpaRepository
 import devcoop.hyunjoon.selfcounter.domain.user.User
-import java.util.Optional
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.*
 
 interface UserRepository : JpaRepository<User, String> {
     fun findByUserCode(userCode: String): Optional<User>
 
     fun countByUserNumberContaining(prefix: String): Long
+
+    fun existsByUserCode(userCode: String): Boolean
+
 }
