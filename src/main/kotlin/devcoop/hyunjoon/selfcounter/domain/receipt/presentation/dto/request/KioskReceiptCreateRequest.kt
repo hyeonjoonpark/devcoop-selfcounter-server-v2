0f0f5acf3f43009dbe1318d1a82308001d83e161
hyeonjoonpark <pjjoon1379@gmail.com>
2024-11-18 23:04:38 +0900

    ADD: Validation Annotation

--- src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/receipt/presentation/dto/request/KioskReceiptCreateRequest.kt
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/receipt/presentation/dto/request/KioskReceiptCreateRequest.kt
@@ -1,8 +1,9 @@
 package devcoop.hyunjoon.selfcounter.domain.receipt.presentation.dto.request
 
 import devcoop.hyunjoon.selfcounter.domain.receipt.presentation.dto.KioskItemInfo
+import jakarta.validation.constraints.NotBlank
 
 data class KioskReceiptCreateRequest(
     val items: MutableList<KioskItemInfo>,
-    val userCode: String,
+    @NotBlank(message = "학생 바코드는 필수값입니다") val userCode: String,
 )
