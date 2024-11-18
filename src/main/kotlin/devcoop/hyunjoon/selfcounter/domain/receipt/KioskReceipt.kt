1486d142d8ee7938354ff01ccce5cd1139c0c705
hyeonjoonpark <pjjoon1379@gmail.com>
2024-11-18 23:10:42 +0900

    FEAT: saleQty default 1로 설정

--- src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/receipt/KioskReceipt.kt
+++ src/main/kotlin/devcoop/hyunjoon/selfcounter/domain/receipt/KioskReceipt.kt
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.CreatedDate
 import java.time.LocalDateTime
 import devcoop.hyunjoon.selfcounter.domain.item.enums.EventType
 import devcoop.hyunjoon.selfcounter.domain.item.enums.SaleType
+import org.hibernate.annotations.ColumnDefault
 import org.hibernate.annotations.DynamicUpdate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 
@@ -16,7 +17,7 @@ class KioskReceipt(
     val itemCode: String,
     val tradedPoint: Int, // 거래 가격
     var itemName: String, // 아이템 이름
-    val saleQty: Int, // 거래 수량
+    @ColumnDefault("1") val saleQty: Int, // 거래 수량
     @CreatedDate val saleDate: LocalDateTime = LocalDateTime.now(), // 거래 발생 시간
     @Enumerated(EnumType.STRING) val saleType: SaleType, // 결제 타입 (NORMAL, REFUND)
     @Enumerated(EnumType.STRING) val eventType: EventType // 이벤트 여부 ('ONE_PLUS_ONE', 'NONE')
